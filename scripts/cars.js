62ceebfe3f81c5740373d538e775a2182b3c2221
Eliza Coc <coc.eliza@utlook.com>
2022-08-04 22:45:04 +0300

    tried to fix custom dropdown (did not work)

--- scripts/cars.js
+++ scripts/cars.js
@@ -13,14 +13,14 @@
 //     });
 // }
 
- function selectPageByIndex(index) {
-     const navigation = document.getElementsByTagName('ul')[0];
-     const itemList = navigation.querySelectorAll('li');
-     // itemList.forEach((item) => {
-     //     item.classList.remove('activ');
-     // })
-     itemList[index].classList.add('activ');
- }
+//  function selectPageByIndex(index) {
+//      const navigation = document.getElementsByTagName('ul')[0];
+//      const itemList = navigation.querySelectorAll('li');
+//      // itemList.forEach((item) => {
+//      //     item.classList.remove('activ');
+//      // })
+//      itemList[index].classList.add('activ');
+//  }
 
 // custom dropdown
 
@@ -37,7 +37,7 @@ const dropdown = () => {
 };
 
 const createInput = () => {
-  const input = document.querySelector(".input");
+  const input = document.getElementById("dropdown-input");
   input.addEventListener("click", toggleDropdown);
 };
 
@@ -66,7 +66,7 @@ const toggleDropdown = () => {
 };
 
 const selectOption = (type) => {
-    const text = document.querySelector('.something');
+    const text = document.getElementById('my-car-type');
     text.textContent = type;
     toggleDropdown();
   };
@@ -77,9 +77,10 @@ dropdown();
 
 const ownerID = ['029318438', '345345624', '325245345'];
 
-const printArea2 = document.getElementById("container2");
+const printArea2 = document.getElementById("container-2");
+
 const dropdown2 = () => {
-  const component = document.getElementById("dropdown2");
+  const component = document.getElementById("dropdown-2");
   const input = createInput2();
   const dropdown = showDropdown2();
   component.appendChild(dropdown);
@@ -87,12 +88,12 @@ const dropdown2 = () => {
 };
 
 const createInput2 = () => {
-  const input = document.querySelector(".input2");
+  const input = document.getElementById("dropdown-input-2");
   input.addEventListener("click", toggleDropdown2);
 };
 
 const showDropdown2 = () => {
-  const structure = document.getElementById("dropdown-structure2");
+  const structure = document.getElementById("dropdown-structure-2");
   structure.classList.add("dd-structure2", "hide");
 
   ownerID.forEach(item => {
@@ -116,35 +117,40 @@ const toggleDropdown2 = () => {
 };
 
 const selectOption2 = (type) => {
-    const text = document.querySelector('.something2');
+    const text = document.getElementById('owner-id');
     text.textContent = type;
     toggleDropdown2();
   };
 
 dropdown2();
 
-// const dropdown = (containerID, dropdownId, dropdownStructure, arrayType, classToggle) => {
+// //general methods
+
+// const carType = ['Diesel', 'Hybrid', 'Electric'];
+// const ownerID = ['029318438', '345345624', '325245345'];
+
+// const dropdown = (containerID, dropdownId, dropdownStructure, arrayType, dropdownInput, spanId) => {
 //     printArea = document.getElementById(containerID);
 //     const component = document.getElementById(dropdownId);
-//     createInput();
-//     const dropdown = showDropdown(dropdownStructure, arrayType, classToggle);
+//     createInput(dropdownInput);
+//     const dropdown = showDropdown(dropdownStructure, arrayType, spanId);
 //     component.appendChild(dropdown);
 //     printArea.appendChild(component);
 //   };
   
-//   const createInput = () => {
-//     const input = document.querySelector(".input");
+//   const createInput = (dropdownInput) => {
+//     const input = document.getElementById(dropdownInput);
 //     input.addEventListener("click", toggleDropdown);
 //   };
   
-//   const showDropdown = (dropdownStructure, arrayType, classToggle) => {
+//   const showDropdown = (dropdownStructure, arrayType, spanId) => {
 //     const structure = document.getElementById(dropdownStructure);
 //     structure.classList.add("dd-structure", "hide");
   
 //     arrayType.forEach(item => {
       
 //       const option = document.createElement("div");
-//   option.addEventListener("click", () => selectOption(item, classToggle));
+//   option.addEventListener("click", () => selectOption(item, spanId));
 //       n = document.createElement("span");
 //       n.textContent = item;
   
@@ -156,18 +162,126 @@ dropdown2();
 //     return structure;
 //   };
   
-//   const toggleDropdown = () => {
+// function toggleDropdown () {
 //     const dropdown = document.querySelector(".dd-structure");
 //     dropdown.classList.toggle("hide");
+//     console.log("done");
 //   };
   
-//   const selectOption = (type, classToggle) => {
-//       const text = document.querySelector(classToggle);
+//   const selectOption = (type, spanId) => {
+//       const text = document.getElementById(spanId);
 //       text.textContent = type;
 //       toggleDropdown();
 //     };
   
-//   dropdown("container","dropdown","dropdown-structure", carType, ".something");
+// dropdown("container","dropdown","dropdown-structure", carType, 'dropdown-input', 'my-car-type');
 
-//   dropdown("container-2","dropdown-2","dropdown-structure-2", ownerID, ".something2");
+// //dropdown("container-2","dropdown-2","dropdown-structure-2", ownerID, 'dropdown-input-2', 'owner-id');
+
+// //general function but with structure id
+
+// const carType = ['Diesel', 'Hybrid', 'Electric'];
+// const ownerID = ['029318438', '345345624', '325245345'];
+
+// const dropdown = (containerID, dropdownId, dropdownStructure, arrayType, dropdownInput, spanId) => {
+//     printArea = document.getElementById(containerID);
+//     const component = document.getElementById(dropdownId);
+//     createInput(dropdownInput);
+//     const dropdown = showDropdown(dropdownStructure, arrayType, spanId);
+//     component.appendChild(dropdown);
+//     printArea.appendChild(component);
+//   };
   
+//   const createInput = (dropdownInput) => {
+//     const input = document.getElementById(dropdownInput);
+//     input.addEventListener("click", toggleDropdown);
+//   };
+  
+//   const showDropdown = (dropdownStructure, arrayType, spanId) => {
+//     const structure = document.getElementById(dropdownStructure);
+//     structure.classList.add("dd-structure", "hide");
+  
+//     arrayType.forEach(item => {
+      
+//       const option = document.createElement("div");
+//   option.addEventListener("click", () => selectOption(item, spanId));
+//       n = document.createElement("span");
+//       n.textContent = item;
+  
+//       option.appendChild(n);
+//       structure.appendChild(option);
+      
+      
+//     });
+//     return structure;
+//   };
+  
+// function toggleDropdown () {
+//     const dropdown = document.getElementById("dropdown-structure");
+//     dropdown.classList.toggle("hide");
+//     console.log("done");
+//   };
+  
+//   const selectOption = (type, spanId) => {
+//       const text = document.getElementById(spanId);
+//       text.textContent = type;
+//       toggleDropdown();
+//     };
+  
+// dropdown("container","dropdown","dropdown-structure", carType, 'dropdown-input', 'my-car-type');
+
+// //dropdown("container-2","dropdown-2","dropdown-structure-2", ownerID, 'dropdown-input-2', 'owner-id');
+
+//current edit
+
+// const carType = ['Diesel', 'Hybrid', 'Electric'];
+// const ownerID = ['029318438', '345345624', '325245345'];
+
+// const dropdown = (containerID, dropdownId, dropdownStructure, arrayType, dropdownInput, spanId) => {
+//     printArea = document.getElementById(containerID);
+//     const component = document.getElementById(dropdownId);
+//     createInput(dropdownInput, dropdownStructure);
+//     const dropdown = showDropdown(dropdownStructure, arrayType, spanId);
+//     component.appendChild(dropdown);
+//     printArea.appendChild(component);
+//   };
+  
+//   const createInput = (dropdownInput, dropdownStructure) => {
+//     const input = document.getElementById(dropdownInput);
+//     input.addEventListener("click", toggleDropdown(dropdownStructure));
+//   };
+  
+//   const showDropdown = (dropdownStructure, arrayType, spanId) => {
+//     const structure = document.getElementById(dropdownStructure);
+//     structure.classList.add("dd-structure", "hide");
+  
+//     arrayType.forEach(item => {
+      
+//       const option = document.createElement("div");
+//   option.addEventListener("click", () => selectOption(item, spanId, dropdownStructure));
+//       n = document.createElement("span");
+//       n.textContent = item;
+  
+//       option.appendChild(n);
+//       structure.appendChild(option);
+      
+      
+//     });
+//     return structure;
+//   };
+  
+// function toggleDropdown (dropdownStructure) {
+//     const dropdown = document.getElementById(dropdownStructure);
+//     dropdown.classList.toggle("hide");
+//     console.log("done");
+//   };
+  
+//   const selectOption = (type, spanId, dropdownStructure) => {
+//       const text = document.getElementById(spanId);
+//       text.textContent = type;
+//       toggleDropdown(dropdownStructure);
+//     };
+  
+// dropdown("container","dropdown","dropdown-structure", carType, 'dropdown-input', 'my-car-type');
+
+// //dropdown("container-2","dropdown-2","dropdown-structure-2", ownerID, 'dropdown-input-2', 'owner-id');
